b85eca0da1ab0382ed368e1f98d7fbfa72f89678
ANUPAM-RO <anupamroy575@gmail.com>
2024-01-20 12:11:11 +0530

    Navbar added

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,11 +1,184 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { setMode, setLogOut } from "state";
+import FlexBeetween from "components/FlexBetween";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  FormControl,
+  IconButton,
+  InputBase,
+  Menu,
+  MenuItem,
+  Select,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { useTheme } from "@emotion/react";
+import {
+  Close,
+  DarkMode,
+  Help,
+  LightMode,
+  Message,
+  Notifications,
+  Search,
+} from "@mui/icons-material";
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const naviagte = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
-export default Navbar
+  const theme = useTheme();
+  const neturalLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user?.firstName} ${user?.lastName}` || "Anupam";
+
+  return (
+    <FlexBeetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBeetween gap="1.7rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem , 2rem, 2.25rem)"
+          color="primary"
+          onClick={() => naviagte("/home")}
+          sx={{ "&:hover": { color: primaryLight, cursor: "pointer" } }}
+        >
+          Readbook
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBeetween
+            backgroundColor={neturalLight}
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBeetween>
+        )}
+      </FlexBeetween>
+
+      {isNonMobileScreens ? (
+        <FlexBeetween gap="2rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkMode sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <Message sx={{ fontSize: "25px" }} />
+          <Notifications sx={{ fontSize: "25px" }} />
+          <Help sx={{ fontSize: "25px" }} />
+          <FormControl variant="standard" value={fullName}>
+            <Select
+              value={fullName}
+              sx={{
+                backgroundColor: neturalLight,
+                width: "150px",
+                borderRadius: "0.25rem",
+                p: "0.25rem 1rem",
+                "& .MuiSvgIcon-root": {
+                  pr: "0.25rem",
+                  width: "3rem",
+                },
+                "& .MuiSelect-select:focus": {
+                  backgroundColor: neturalLight,
+                },
+              }}
+              input={<InputBase />}
+            >
+              <MenuItem value={fullName}>
+                <Typography>{fullName}</Typography>
+              </MenuItem>
+              <MenuItem onClick={() => dispatch(setLogOut())}>Log Out</MenuItem>
+            </Select>
+          </FormControl>
+        </FlexBeetween>
+      ) : (
+        <IconButton
+          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+        >
+          <Menu />
+        </IconButton>
+      )}
+
+      {/* MOBILE NAV */}
+      {!isNonMobileScreens && isMobileMenuToggled && (
+        <Box
+          position="fixed"
+          right="0"
+          bottom="0"
+          height="100%"
+          zIndex="10"
+          maxWith="500px"
+          backgroundColor={background}
+        >
+          <Box display="flex" justifyContent="flex-end" p="1rem">
+            <IconButton
+              onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+            >
+              <Close />
+            </IconButton>
+          </Box>
+          {/* MENU ITEMS */}
+          <FlexBeetween
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="3rem"
+          >
+            <IconButton onClick={() => dispatch(setMode())}>
+              {theme.palette.mode === "dark" ? (
+                <DarkMode sx={{ fontSize: "25px" }} />
+              ) : (
+                <LightMode sx={{ color: dark, fontSize: "25px" }} />
+              )}
+            </IconButton>
+            <Message sx={{ fontSize: "25px" }} />
+            <Notifications sx={{ fontSize: "25px" }} />
+            <Help sx={{ fontSize: "25px" }} />
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neturalLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neturalLight,
+                  },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => dispatch(setLogOut())}>
+                  Log Out
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBeetween>
+        </Box>
+      )}
+    </FlexBeetween>
+  );
+};
+
+export default Navbar;
